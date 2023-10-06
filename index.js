7ea6a1fcf077b9b6ae7b5e15c2dd0a4cd9f24a39
Hariprasathnagaraj <harisharan81.hs@gmail.com>
2023-10-06 16:03:16 +0530

    hari

---
+++ index.js
@@ -0,0 +1,52 @@
+// let a =25;
+// console.log(a);
+
+
+// const student ={"name":"ram","age":12};
+// console.log(student.name);
+// student.name="hari"
+// console.log(student)
+
+// let a = "35";
+// console.log(a,typeof a);
+// a = parseFloat(a);
+// console.log(a,typeof a);
+
+// let a = "25";
+// let b =10;
+// console.log(a+b);
+
+// a = Number(a)
+// console.log(a+b);
+
+
+// let a = 100;
+// b=20;
+// let c;
+
+// c = --a;
+// console.log(c)
+
+// let a=10;
+
+// a+=5;
+
+// console.log(a);
+
+
+let c;
+
+c=Math.floor(5.9); // output - 5
+c=Math.round(5.1);  //5
+c=Math.round(5.5);  //6
+c=Math.ceil(5.1);//6
+c=Math.sqrt(90);  
+c=Math.abs(-85);   // minus value kudutha plus valu kamikim ex:85
+c=Math.trunc(2.58); // decimal to integer ans: 2
+c=Math.pow(2,4);  //4*4 = 16
+c=Math.random();  // it will change everytym when we click the button
+c=Math.floor((Math.random()*50+1)); // it will change everytym when we click the button but it will be below 50 
+c=Math.sign(-25) // negative value means -1 positive value means 1 and 0 will be 0
+console.log(c);
+
+
